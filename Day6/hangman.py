8b1eb1e3bc18cf247c0604724d16dc276566b3a4
sagacandy <sagarkht@protonmail.com>
2023-03-27 04:33:22 +0545

    Day6 added some ui changes and used modules to organize the code

--- Day6/hangman.py
+++ Day6/hangman.py
@@ -1,86 +1,31 @@
 import random
+import module
+import words
 
-# Define the word list and the hangman stages
-word_list = ["aardvark", "baboon", "camel"]
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+print(module.hangman)
+words.words  # module defined in words.py
 
 # Select a random word and initialize the game state
-chosen_word = random.choice(word_list)
-word_length = len(chosen_word)
-display = ["_"] * word_length
+chosen_word = random.choice(words.words)
+words.words = len(chosen_word)
+display = ["_"] * words.words
 chosen_word_list = list(chosen_word)
 
 # Set the maximum number of incorrect guesses and initialize the incorrect guess counter
-max_incorrect_guesses = len(stages) - 1
+max_incorrect_guesses = len(module.stages) - 1
 incorrect_guesses = 0
 
 # Loop until the player wins or loses
 while chosen_word_list != display and incorrect_guesses < max_incorrect_guesses:
     guess = input("Enter your guess letter: ").lower()
     if guess in chosen_word_list:
-        for position in range(word_length):
+        for position in range(words.words):
             if chosen_word_list[position] == guess:
                 display[position] = guess
     else:
         incorrect_guesses += 1
-        print(stages[incorrect_guesses])
+        print(module.stages[incorrect_guesses])
     print(display)
-
 # Check if the player won or lost
 if incorrect_guesses == max_incorrect_guesses:
     print("Game over. You ran out of lives. The word was", chosen_word)
---
+++ Day6/words.py
@@ -0,0 +1,3 @@
+words = ['apple', 'banana', 'cherry', 'dog', 'elephant', 'fox', 'grape', 'horse', 'igloo', 'jacket', 'kiwi', 'lion', 'monkey',
+         'noodle', 'orange', 'pineapple', 'queen', 'rabbit', 'snake', 'tiger', 'umbrella', 'violin', 'watermelon', 'xylophone', 'zebra']
+print(words)
